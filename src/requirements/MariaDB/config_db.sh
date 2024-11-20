03a42765cda067cd0a36bf48fd7f803e475abbf9
JFikents <138692812+JorFik@users.noreply.github.com>
2024-11-20 14:40:06 +0100

    Add environment variables for WordPress and update database script variable names

--- src/requirements/MariaDB/config_db.sh
+++ src/requirements/MariaDB/config_db.sh
@@ -2,10 +2,10 @@
 
 source /run/secrets/credentials
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > wp_db.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> wp_db.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> wp_db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$root_pass' ;" >> wp_db.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > wp_db.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD' ;" >> wp_db.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%' ;" >> wp_db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD' ;" >> wp_db.sql
 echo "FLUSH PRIVILEGES;" >> wp_db.sql
 
 mysql < wp_db.sql
