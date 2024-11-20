59446722adf2adada8e28c459d2893982cf34370
Jorge Fikentscher <138692812+JorFik@users.noreply.github.com>
2024-11-20 20:34:55 +0100

    Fix order
    Script cant use secrets at buildtime

--- src/requirements/MariaDB/config_db.sh
+++ src/requirements/MariaDB/config_db.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-source /run/secrets/credentials
+if [ -f /run/secrets/credentials ]; then
+	source /run/secrets/credentials
+else
+    echo "Credentials file not found"
+	cat /run/secrets/credentials
+    exit 1
+fi
+
+service mysql start
+
+sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/my.cnf
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > wp_db.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD' ;" >> wp_db.sql
@@ -11,3 +21,5 @@ echo "FLUSH PRIVILEGES;" >> wp_db.sql
 mysql < wp_db.sql
 
 rm wp_db.sql
+
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql
