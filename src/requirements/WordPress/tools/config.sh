431f4ed5d68c9eff4eaf2cfcbe3d320bddde5672
Jorge Fikentscher <138692812+JorFik@users.noreply.github.com>
2024-11-20 20:35:23 +0100

    Fix script

--- src/requirements/WordPress/tools/config.sh
+++ src/requirements/WordPress/tools/config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -7,13 +7,18 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --path=/var/www/html --allow-root
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-# Change placeholders in wp-config.php
-source /run/secrets/credentials
+if [ -f /run/secrets/credentials ]; then
+    source /run/secrets/credentials
+else
+    echo "Credentials file not found"
+    exit 1
+fi
 
-sed -i -r "s/database/$DB_NAME/1"		wp-config.php
-sed -i -r "s/database_user/$DB_USR/1"	wp-config.php
-sed -i -r "s/password/$DB_PWD/1"		wp-config.php
-sed -i -r "s/localhost/mariadb/1"		wp-config.php
+# Change placeholders in wp-config.php
+sed -i -r "s/wordpress_name_here/$DB_NAME/1" 	wp-config.php
+sed -i -r "s/username_here/$DB_USR/1" 			wp-config.php
+sed -i -r "s/password_here/$DB_PWD/1" 			wp-config.php
+sed -i -r "s/localhost/mariadb/1" 				wp-config.php
 
 # Set up Admin user
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
@@ -26,7 +31,9 @@ wp theme install twentytwentytwo --activate --allow-root
 # Set up plugins
 wp plugin install redis-cache --activate --allow-root
 
-sed -i 's/listen = \/run\/php\/php83-fpm.sock/listen = 9000/g' /etc/php/8/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php82-fpm.sock/listen = 9000/g' /etc/php82/php-fpm.d/www.conf
 
 # Enable Redis
 wp redis enable --allow-root
+
+/usr/sbin/php-fpm82 -F
