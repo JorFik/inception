2991afc86d00ee481094f908ebe80201564b5d11
JFikents <138692812+JorFik@users.noreply.github.com>
2024-11-25 00:47:06 +0100

    Added nginx config
    First complete draft

---
+++ src/requirements/NGINX/tools/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/ssl/private/nginx-selfsigned.key \
+	-out /etc/ssl/certs/nginx-selfsigned.crt \
+	-subj "/C=DE/L=Heilbronn/O=42 Heilbronn/CN=JFikents"
+
+sed -i -r 's/DOMAIN_NAME/'$DOMAIN_NAME'/g' /etc/nginx/http.d/default.conf
+
+exec nginx -g 'daemon off;'
