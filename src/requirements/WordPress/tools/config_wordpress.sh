6bdb273f004eeaa311168015cc6d99c0d278639d
JFikents <138692812+JorFik@users.noreply.github.com>
2024-11-24 18:14:45 +0100

    Refactor WordPress setup scripts for improved organization and clarity

--- src/requirements/WordPress/tools/config_wordpress.sh
+++ src/requirements/WordPress/tools/config_wordpress.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-if [ -z "$(ls -A /var/www/html)" ]; then
-    wp core download --path=/var/www/html --allow-root
-    mv /tmp/wp-config.php /var/www/html/wp-config.php
-    rm /var/www/html/wp-config-sample.php
-else
-    echo "WordPress is already installed."
-fi
-
 if [ -f /run/secrets/credentials ]; then
     source /run/secrets/credentials
 else
@@ -18,35 +7,60 @@ else
     exit 1
 fi
 
-# Change placeholders in wp-config.php
-sed -i -r "s/database_name_here/$DB_NAME/1" 	wp-config.php
-sed -i -r "s/username_here/$DB_USR/1" 			wp-config.php
-sed -i -r "s/password_here/$DB_PWD/1" 			wp-config.php
-sed -i -r "s/localhost/src_mariadb_1:3306/1" 	wp-config.php
+install_wp_cli()
+{
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+}
 
-# Fetch unique keys and salts
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ > salts.txt
+execute_php_in_port_9000()
+{
+    sed -i 's/listen = \/run\/php\/php82-fpm.sock/listen = 9000/g' /etc/php82/php-fpm.d/www.conf
+    exec /usr/sbin/php-fpm82 -F
+}
 
-# Insert keys and salts into wp-config.php
-sed -i '/insert_keys_and_salts_here/r salts.txt' wp-config.php
-sed -i '/insert_keys_and_salts_here/d' wp-config.php
+prepare_wp_config_php()
+{
+    mv /tmp/wp-config.php /var/www/html/wp-config.php
+    rm /var/www/html/wp-config-sample.php
 
-rm salts.txt
+    sed -i -r "s/database_name_here/$DB_NAME/1" 	wp-config.php
+    sed -i -r "s/username_here/$DB_USR/1" 			wp-config.php
+    sed -i -r "s/password_here/$DB_PWD/1" 			wp-config.php
+    sed -i -r "s/localhost/src_mariadb_1:3306/1" 	wp-config.php
 
-# Set up Admin user
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-# Set up Author user
-wp user create $WP_USR $WP_USR_EMAIL --role=author --user_pass=$WP_USR_PWD --allow-root
+    # Fetch unique keys and salts
+    curl -s https://api.wordpress.org/secret-key/1.1/salt/ > salts.txt
 
-# Set theme
-wp theme install twentytwentytwo --activate --allow-root
+    # Insert keys and salts into wp-config.php
+    sed -i '/insert_keys_and_salts_here/r salts.txt' wp-config.php
+    sed -i '/insert_keys_and_salts_here/d' wp-config.php
 
-# Set up plugins
-# wp plugin install redis-cache --activate --allow-root
+    rm salts.txt
+}
 
-sed -i 's/listen = \/run\/php\/php82-fpm.sock/listen = 9000/g' /etc/php82/php-fpm.d/www.conf
+setup_wp()
+{
+    install_wp_cli
+    wp core download --path=/var/www/html --allow-root
+    prepare_wp_config_php
 
-# Enable Redis
-# wp redis enable --allow-root
+    wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    wp user create $WP_USR $WP_USR_EMAIL --role=author --user_pass=$WP_USR_PWD --allow-root
+    wp theme install twentytwentytwo --activate --allow-root
 
-exec /usr/sbin/php-fpm82 -F
+    # Set up plugins
+    # wp plugin install redis-cache --activate --allow-root
+
+    # Enable Redis
+    # wp redis enable --allow-root
+}
+
+if [ ! -z "$(ls -A /var/www/html)" ]; then
+    echo "WordPress is already installed."
+else
+    setup_wp
+fi
+
+execute_php_in_port_9000
