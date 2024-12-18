db591ba871e1363095ff56373b897eabb9f33d03
JFikents <138692812+JorFik@users.noreply.github.com>
2024-12-17 17:54:37 -0800

    Finish first draft

--- src/requirements/NGINX/tools/setup.sh
+++ src/requirements/NGINX/tools/setup.sh
@@ -7,4 +7,6 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 
 sed -i -r 's/DOMAIN_NAME/'$DOMAIN_NAME'/g' /etc/nginx/http.d/default.conf
 
+chown -R nobody:nobody /var/www/html
+
 exec nginx -g 'daemon off;'
